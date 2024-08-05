833dd1b660651f80b32224625d856c3dd48ae223
BrainiacRawkib <brainiacrawkib@gmail.com>
2024-08-05 12:00:52 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,49 +1,42 @@
 import mysql.connector
- 
+
 import pymongo
  
 delete_existing_documents = True
- 
-mysql_host="localhost"
- 
-mysql_database="mydatabase"
- 
+
 mysql_schema = "myschema"
- 
-mysql_user="myuser"
- 
-mysql_password="********"
- 
+
 mongodb_host = "mongodb://localhost:27017/"
- 
+
 mongodb_dbname = "mymongodb"
- 
+
 mysqldb = mysql.connector.connect(
- 
-   host=mysql_host,
- 
-   database=mysql_database,
- 
-   user=mysql_user,
- 
-   password=mysql_password
- 
+    host='localhost',
+    user='myuser',
+    password='********',
+    database='mydatabase',
 )
 
-mycursor = mysqldb.cursor(dictionary=True)
- 
-mycursor.execute("SELECT * from categories;")
- 
-myresult = mycursor.fetchall()
- 
+cursor = mysqldb.cursor(dictionary=True)
+
+# retrieve all tables
+
+tables = cursor.execute('SHOW TABLES')
+
+for table in tables:
+    print(f'Table ====> {table}')
+
+
+cursor.execute("SELECT * from categories;")
+
+myresult = cursor.fetchall()
+
 myclient = pymongo.MongoClient(mongodb_host)
- 
+
 mydb = myclient[mongodb_dbname]
- 
+
 mycol = mydb["categories"]
- 
+
 if len(myresult) > 0:
- 
        x = mycol.insert_many(myresult) #myresult comes from mysql cursor
- 
        print(len(x.inserted_ids))
