b48b1c151b45993302c28545412c1ba3f1cbd7e0
BrainiacRawkib <brainiacrawkib@gmail.com>
2024-08-06 00:37:41 +0100

    updates
    Please enter the commit message for your changes. Lines starting

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import argparse
+
 import mysql.connector
 
 import pymongo
@@ -10,33 +12,49 @@ mongodb_host = "mongodb://localhost:27017/"
 
 mongodb_dbname = "mymongodb"
 
-mysqldb = mysql.connector.connect(
-    host='localhost',
-    user='myuser',
-    password='********',
-    database='mydatabase',
-)
-
-cursor = mysqldb.cursor(dictionary=True)
-
-# retrieve all tables
-
-tables = cursor.execute('SHOW TABLES')
-
-for table in tables:
-    print(f'Table ====> {table}')
-
-
-cursor.execute("SELECT * from categories;")
-
-myresult = cursor.fetchall()
-
 myclient = pymongo.MongoClient(mongodb_host)
 
 mydb = myclient[mongodb_dbname]
 
 mycol = mydb["categories"]
 
-if len(myresult) > 0:
-       x = mycol.insert_many(myresult) #myresult comes from mysql cursor
-       print(len(x.inserted_ids))
+# mysqldb = mysql.connector.connect(
+#         host='localhost',
+#         user='myuser',
+#         password='********',
+#         database='mydatabase',
+#     )
+
+def db_migrate(mysqldb_host: str, mysqldb_user: str, mysqldb_password: str, mysqldb_database: str, mongodb_conn_string: str):
+    mysqldb = mysql.connector.connect(
+        host=mysqldb_host,
+        user=mysqldb_user,
+        password=mysqldb_password,
+        database=mysqldb_database
+    )
+
+    cursor = mysqldb.cursor(dictionary=True)
+
+    # retrieve all tables
+    tables = cursor.execute('SHOW TABLES')
+
+    for table in tables:
+        print(f'Processing table ====> {table}')
+
+    results = cursor.fetchall()
+
+    if results:
+        x = mycol.insert_many(results)  # results comes from mysql cursor
+        print(len(x.inserted_ids))
+
+
+def run_db_migration():
+    parser = argparse.ArgumentParser(description="ETL Script to migrate data from MySQL to MongoDB")
+    parser.add_argument('mysqldb_connection_string', help="MySQL Database connection string")
+    parser.add_argument('mongodb_connection_string', help="MongoDB connection string")
+
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    run_db_migration()
