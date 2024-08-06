3e6067cef9d63e6896bb783b4ca248dd7e2023c6
BrainiacRawkib <brainiacrawkib@gmail.com>
2024-08-06 08:23:31 +0100

    updates

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import argparse
+import sys
 
 import mysql.connector
 
-import pymongo
+from mysql.connector import errorcode
+from pymongo import MongoClient
  
 delete_existing_documents = True
 
@@ -12,7 +14,7 @@ mongodb_host = "mongodb://localhost:27017/"
 
 mongodb_dbname = "mymongodb"
 
-myclient = pymongo.MongoClient(mongodb_host)
+myclient = MongoClient(mongodb_host)
 
 mydb = myclient[mongodb_dbname]
 
@@ -25,27 +27,48 @@ mycol = mydb["categories"]
 #         database='mydatabase',
 #     )
 
-def db_migrate(mysqldb_host: str, mysqldb_user: str, mysqldb_password: str, mysqldb_database: str, mongodb_conn_string: str):
-    mysqldb = mysql.connector.connect(
-        host=mysqldb_host,
-        user=mysqldb_user,
-        password=mysqldb_password,
-        database=mysqldb_database
-    )
+def db_migrate(mysqldb_host: str, mysqldb_user: str, mysqldb_password: str, mysqldb_database: str, mongodb_connection_uri: str):
+    # connect to MySQL DB
+    try:
+        mysqldb = mysql.connector.connect(
+            host=mysqldb_host,
+            user=mysqldb_user,
+            password=mysqldb_password,
+            database=mysqldb_database
+        )
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Invalid username or password')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print('Database does not exist')
+        else:
+            print(err)
+        sys.exit(1)
 
+    # connect to MongoDB
+    mongodb_client = MongoClient(mongodb_connection_uri)
+
+    # get or create database in mongodb
+    mongodb_client_db = mongodb_client[mysqldb_database]
+
+    # instantiate MySQL DB cursor
     cursor = mysqldb.cursor(dictionary=True)
 
     # retrieve all tables
     tables = cursor.execute('SHOW TABLES')
 
-    for table in tables:
-        print(f'Processing table ====> {table}')
+    for table_name in tables:
+        print(f'Processing table ====> {table_name}')
+        # create and populate the collections
+        collection = mongodb_client_db[table_name]
 
-    results = cursor.fetchall()
+        results = cursor.execute(f"SELECT * FROM {table_name}")
+        if results:
+            paginated_results = cursor.fetchmany(100)
 
-    if results:
-        x = mycol.insert_many(results)  # results comes from mysql cursor
-        print(len(x.inserted_ids))
+            # bulk insert data into mongodb
+            inserted_data = collection.insert_many(paginated_results)
+            print(len(inserted_data.inserted_ids))
 
 
 def run_db_migration():
